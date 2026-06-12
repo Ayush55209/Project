9545889109af61a1600bf5ea1d92045e07b003eb
Ayush55209 <ayushpandey.1138@gmail.com>
2026-06-12 11:29:25 +0530

    DSA

---
+++ C++ DSA/C++/Operators/operators2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     bool expl1=true;
+     bool expl2=false;
+
+     cout<<(expl1 && expl2)<<endl; //false
+     cout<<(expl1 || expl2)<<endl; //true
+     cout<<(!expl1)<<endl; //false
+
+     return 0;
+
+}
