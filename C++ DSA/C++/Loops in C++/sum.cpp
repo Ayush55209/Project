100c6c253fd0d8cb246e18ce24c056d86b79eb48
Ayush55209 <ayushpandey.1138@gmail.com>
2026-06-13 16:17:29 +0530

    loops

--- C++ DSA/C++/Loops in C++/sum.cpp
+++ C++ DSA/C++/Loops in C++/sum.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        sum += i;
+    }
+
+    cout << "The sum of the first 10 natural numbers is: " << sum << endl;
+
+    return 0;
+}
