64f530a570849926774945684ea12aae55f7d11b
Ayush55209 <ayushpandey.1138@gmail.com>
2026-06-13 16:27:07 +0530

    age

--- C++ DSA/C++/Conditionals/agegroup.cpp
+++ C++ DSA/C++/Conditionals/agegroup.cpp
@@ -1,21 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int age;
-    cin>>age;
+    cin >> age;
 
-    if(age<12){
-        cout<<"Child"<<endl;
+    if (age < 12)
+    {
+        cout << "Child" << endl;
     }
-    else if(age>18){
-        cout<<"Adult"<<endl;
+    else if (age > 18)
+    {
+        cout << "Adult" << endl;
     }
-    else {
-        cout<<"Teenager"<<endl;
+    else
+    {
+        cout << "Teenager" << endl;
     }
-   
 
     return 0;
-} 
+}
